afc8f9b9dda50bc97f2507b4569275db3e81fe72
東島子竜 <s1f102103460@iniad.org>
2023-07-27 05:23:42 +0900

    Plantボタンを設置 動くよ!

--- src/github.js
+++ src/github.js
@@ -1,14 +1,22 @@
-function plantGrass (){
+// document.getElementById("grassButton").addEventListener("click", function() {
+//     let elements = document.querySelectorAll('[data-level]');
+//     for (let i = 0; i < elements.length; i++) {
+//         elements[i].dataset.level = Math.floor(Math.random() * 4);
+// }   
+//     console.log("hoge");
+// });
+
+let parentElement = document.getElementById("year-list-container");
+let buttonElement = document.createElement("button");
+buttonElement.addEventListener("click", function() {
     let elements = document.querySelectorAll('[data-level]');
     for (let i = 0; i < elements.length; i++) {
         elements[i].dataset.level = Math.floor(Math.random() * 4);
-}   
-}
-
-
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("grassButton").addEventListener("click", function() {
-      plantGrass();
-    });
-  });
-  
+    }   
+});
+buttonElement.innerText = "Plant!";
+buttonElement.style.borderRadius = "5px";
+buttonElement.style.background = "#1f71eb";
+buttonElement.style.border = "none";
+parentElement.appendChild(buttonElement);
+console.log("イメージするのは常に最強の自分");
